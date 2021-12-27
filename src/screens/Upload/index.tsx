dd4c35328000387bf64df2769fcff5e9e338e5fd
Paulo Filho <pcf.feat@gmail.com>
2021-12-27 13:12:54 -0400

    feat: monitor upload progress

--- src/screens/Upload/index.tsx
+++ src/screens/Upload/index.tsx
@@ -11,6 +11,8 @@ import { Alert } from 'react-native';
 
 export function Upload() {
   const [image, setImage] = useState('');
+  const [bytesTransferred, setBytesTransferred] = useState('');
+  const [progress, setProgress] = useState('0');
 
   async function handlePickImage() {
     const { status } = await ImagePicker.requestMediaLibraryPermissionsAsync();
@@ -32,12 +34,24 @@ export function Upload() {
     const fileName = new Date().getTime();
     const reference = storage().ref(`/images/${fileName}.png`);
 
-    reference
-      .putFile(image)
-      .then(() => {
-        Alert.alert('Upload concluído!');
-      })
-      .catch(error => console.error(error));
+    const uploadTask = reference.putFile(image);
+
+    uploadTask.on('state_changed', taskSnapshot => {
+      const percent = (
+        (taskSnapshot.bytesTransferred / taskSnapshot.totalBytes) *
+        100
+      ).toFixed(0);
+
+      setProgress(percent);
+
+      setBytesTransferred(
+        `${taskSnapshot.bytesTransferred} transferido de ${taskSnapshot.totalBytes}`
+      );
+    });
+
+    uploadTask.then(() => {
+      Alert.alert('Upload concluído!');
+    });
   }
 
   return (
@@ -49,9 +63,9 @@ export function Upload() {
 
         <Button title='Fazer upload' onPress={handleUpload} />
 
-        <Progress>0%</Progress>
+        <Progress>{progress}%</Progress>
 
-        <Transferred>0 de 100 bytes transferido</Transferred>
+        <Transferred>{bytesTransferred}</Transferred>
       </Content>
     </Container>
   );
