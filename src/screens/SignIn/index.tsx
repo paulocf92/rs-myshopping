cda9ef1c64c81eccac13bb9914618bd3e379ff24
Paulo Filho <pcf.feat@gmail.com>
2021-12-27 15:22:10 -0400

    feat: handle errors on account creation

--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -19,7 +19,22 @@ export function SignIn() {
   function handleCreateUserAccount() {
     auth()
       .createUserWithEmailAndPassword(email, password)
-      .then(() => Alert.alert('Usuário criado com sucesso!'));
+      .then(() => Alert.alert('Usuário criado com sucesso!'))
+      .catch(error => {
+        console.log(error.code);
+
+        if (error.code === 'auth/email-already-in-use') {
+          return Alert.alert('E-mail indisponível. Escolha outro e-mail.');
+        }
+
+        if (error.code === 'auth/invalid-email') {
+          return Alert.alert('E-mail inválido.');
+        }
+
+        if (error.code === 'auth/weak-password') {
+          return Alert.alert('A senha deve conter pelo menos 6 dígitos.');
+        }
+      });
   }
 
   return (
