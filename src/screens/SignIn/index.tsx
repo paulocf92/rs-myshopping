aa186ff7fdc2f65175bcac831d78ee938e76a4ed
Paulo Filho <pcf.feat@gmail.com>
2021-12-27 15:32:20 -0400

    feat: handle errors on sign-in

--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -37,9 +37,22 @@ export function SignIn() {
       });
   }
 
-  async function handleSignInWithEmailAndPassword() {
-    const { user } = await auth().signInWithEmailAndPassword(email, password);
-    console.log({ user });
+  function handleSignInWithEmailAndPassword() {
+    auth()
+      .signInWithEmailAndPassword(email, password)
+      .then(({ user }) => console.log({ user }))
+      .catch(error => {
+        console.log(error.code);
+
+        if (
+          error.code === 'auth/user-not-found' ||
+          error.code === 'auth/wrong-password'
+        ) {
+          return Alert.alert(
+            'Usuário não encontrado. E-mail e/ou senha inválida!'
+          );
+        }
+      });
   }
 
   return (
