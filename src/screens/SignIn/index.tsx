782745e4f607344f2e36a92afd8f911d03d1c86d
Paulo Filho <pcf.feat@gmail.com>
2021-12-27 14:31:01 -0400

    feat: preview anonymous sign-in strategy

--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import auth from '@react-native-firebase/auth';
 
 import { Container, Account, Title, Subtitle } from './styles';
 import { ButtonText } from '../../components/ButtonText';
@@ -6,25 +7,25 @@ import { Button } from '../../components/Button';
 import { Input } from '../../components/Input';
 
 export function SignIn() {
+  async function handleSignInAnonymously() {
+    const { user } = await auth().signInAnonymously();
+    console.log({ user });
+  }
+
   return (
     <Container>
       <Title>MyShopping</Title>
       <Subtitle>monte sua lista de compra te ajudar nas compras</Subtitle>
 
-      <Input
-        placeholder="e-mail"
-      />
+      <Input placeholder='e-mail' />
 
-      <Input
-        placeholder="senha"
-        keyboardType="email-address"
-      />
+      <Input placeholder='senha' keyboardType='email-address' />
 
-      <Button title="Entrar" onPress={() => { }} />
+      <Button title='Entrar' onPress={handleSignInAnonymously} />
 
       <Account>
-        <ButtonText title="Recuperar senha" onPress={() => { }} />
-        <ButtonText title="Criar minha conta" onPress={() => { }} />
+        <ButtonText title='Recuperar senha' onPress={() => {}} />
+        <ButtonText title='Criar minha conta' onPress={() => {}} />
       </Account>
     </Container>
   );
