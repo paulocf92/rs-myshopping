a0a92453858a916ab5d338097735920b73243214
Paulo Filho <pcf.feat@gmail.com>
2021-12-27 13:14:24 -0400

    fix: fix picture upload screen title

--- src/screens/Upload/index.tsx
+++ src/screens/Upload/index.tsx
@@ -56,7 +56,7 @@ export function Upload() {
 
   return (
     <Container>
-      <Header title='Lista de compras' />
+      <Header title='Upload de Fotos' />
 
       <Content>
         <Photo uri={image} onPress={handlePickImage} />
