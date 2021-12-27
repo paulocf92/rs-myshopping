738bcc656f2d11cc9febb508b5ebb0cbd9edd586
Paulo Filho <pcf.feat@gmail.com>
2021-12-27 11:07:00 -0400

    chore: store a new document to firestore with a custom id

--- src/components/FormBox/index.tsx
+++ src/components/FormBox/index.tsx
@@ -13,7 +13,8 @@ export function FormBox() {
   async function handleProductAdd() {
     firestore()
       .collection('products')
-      .add({ description, quantity, done: false })
+      .doc('my-custom-id')
+      .set({ description, quantity, done: false })
       .then(() => Alert.alert('Produto adicionado com sucesso!'))
       .catch(error => console.error(error));
   }
